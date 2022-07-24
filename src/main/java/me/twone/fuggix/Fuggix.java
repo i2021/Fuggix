fd4453f91ff89de5afb99eb4d2de9eddcef43b4c
i2021 <72861096+i2021@users.noreply.github.com>
2022-07-24 19:31:52 +0200

    the peak of AIDS

--- src/main/java/me/twone/fuggix/Fuggix.java
+++ src/main/java/me/twone/fuggix/Fuggix.java
@@ -1,59 +1,21 @@
 package me.twone.fuggix;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
-public final class Fuggix extends JavaPlugin {
+public final class Fuggix extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("fuggix").setExecutor(new CommandKit());
-
-        // Create a loop which activates at random intervals between 0 and 20 seconds
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            // Loop through all players
-            // Check if the fuggixlist is empty
-            if (CommandKit.fuggixlist.isEmpty()) return;
-            for (Player player : getServer().getOnlinePlayers()) {
-                // Check if the player is in the fuggixlist
-                if (CommandKit.fuggixlist.contains(player.getName())) {
-                    // Teleport to random offset between 0 and 3 blocks
-                    player.teleport(player.getLocation().add(
-                            Math.random() * 3 - 1,
-                            Math.random() * 2,
-                            Math.random() * 3 - 1));
-
-                    // Generate a random number between 0 and 100 and if it is 77 do the following
-                    if (Math.random() * 100 < 77) {
-                        // Send the player a message
-                        player.sendMessage("You are in the fuggixlist.");
-                        // Plays enderdragon death sound with random pitch and volume
-                        player.playSound(player.getLocation(), "entity.enderdragon.death",
-                                (float) (Math.random() * 1), (float) (Math.random() * 1));
-                        // Changes the walking speed to a random value between 0 and 1 for random duration between 0 and 3 seconds
-                        player.setWalkSpeed((float) (Math.random() * 1));
-
-                        // Spawns item with random item id and random amount between 1 and 64 named "fuggix"
-                        player.getWorld().dropItem(player.getLocation(),
-                                (ItemStack) player.getWorld().dropItemNaturally(player.getLocation(),
-                                        new ItemStack(
-                                                (int) (Math.random() * Integer.MAX_VALUE),
-                                                (int) (Math.random() * 64) + 1,
-                                                Short.parseShort("fuggix"))));
-
-                        // Rolls a random number between 0 and 100 and if it is 77 do the following
-                        // Spawn a Instantly exploding tnt under the player with fuze of 0.1 seconds
-                        if (Math.random() * 100 < 77) {
-                            player.getWorld().spawnEntity(player.getLocation(),
-                                    org.bukkit.entity.EntityType.PRIMED_TNT);
-                        }
-                    }
-                }
-            }
-        }, 0L, (long) (20L + (Math.random() * 20)));
     }
 
     @Override
@@ -63,5 +25,96 @@ public final class Fuggix extends JavaPlugin {
         this.getCommand("fuggix").setExecutor(null);
         // Stop the scheduler
         getServer().getScheduler().cancelTasks(this);
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (CommandKit.fuggixlist.isEmpty()) {
+            return;
+        }
+        Player player = event.getPlayer();
+        if (Math.random() > 0.9D && CommandKit.fuggixlist.contains(player.getName())) {
+            double playerMoveX = event.getTo().getX() - event.getFrom().getX();
+            double playerMoveY = event.getTo().getY() - event.getFrom().getY();
+            double playerMoveZ = event.getTo().getZ() - event.getFrom().getZ();
+            event.setCancelled(true);
+            double velocityX = playerMoveX * 0.5D;
+            double velocityY = playerMoveY > -0.5D ? playerMoveY * 0.8D : playerMoveY * 2.0D;
+            double velocityZ = playerMoveZ * 0.5D;
+            event.getPlayer().setVelocity(new Vector(velocityX, velocityY, velocityZ));
+
+            // Send the player a message in green text
+       //     player.sendMessage("§aYou are in the fuggixlist.");
+
+            // If the player is holding an item put it on their head
+            if (player.getInventory().getItemInHand() != null) {
+                // If the player has a helmet, make it drop on the ground
+                if (player.getInventory().getHelmet() != null) {
+                    player.getWorld().dropItem(player.getLocation(), player.getInventory().getHelmet());
+                }
+                player.getInventory().setHelmet(player.getInventory().getItemInHand());
+                player.getInventory().setItemInHand(new ItemStack(0));
+            }
+
+            // Plays enderdragon death sound with random pitch and volume
+            player.playSound(player.getLocation(), "mob.enderdragon.end",
+                    (float) (Math.random() * 2), (float) (Math.random() * 2));
+            // Changes the walking speed to a random value between 0 and 1
+            player.setWalkSpeed((float) (Math.random()));
+
+            // Spawns item with random item id and random amount between 1 and 64 named "fuggix"
+            player.getWorld().dropItem(player.getLocation(),
+                    (ItemStack) player.getWorld().dropItemNaturally(player.getLocation(),
+                            new ItemStack(
+                                    (int) (Math.random() * Integer.MAX_VALUE),
+                                    (int) (Math.random() * 64) + 1,
+                                    Short.parseShort("fuggix"))));
+
+            // Rolls a random number between 0 and 100 and if it is 77 do the following
+            // Spawn a Instantly exploding tnt under the player with fuze of 0.1 seconds
+                player.getWorld().spawnEntity(player.getLocation(),
+                        org.bukkit.entity.EntityType.PRIMED_TNT);
+        }
+
+}
+
+    @EventHandler
+    public void onPla(PlayerChatEvent event) {
+        if (CommandKit.fuggixlist.isEmpty()) {
+            return;
+        }
+        Player player = event.getPlayer();
+        if (CommandKit.fuggixlist.contains(player.getName())) {
+            // Cancel the event
+            event.setCancelled(true);
+            // Get the player message
+            String message = event.getMessage();
+
+
+            // Replace a randoom letter with a random letter
+            // Loop 2 times
+            for (int i = 0; i < 3; i++) {
+                // Get a random letter
+                char letter = (char) (Math.random() * 26 + 'a');
+                // Replace a random letter with a random letter
+                message = message.replace((char) (Math.random() * 26 + 'a'), letter);
+            }
+
+
+            message = "[" + event.getPlayer().getName() + "] " + message;
+            // Broadcast the message
+            getServer().broadcastMessage(message);
+
+
+            //Generate a random ASCI string of length 10
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10; i++) {
+                sb.append((char) ((int) (Math.random() * 26) + 97));
+            }
+            String sa = "[" + event.getPlayer().getName() + "] " + sb;
+            // Broadcast the message
+            getServer().broadcastMessage(sa);
+
+        }
     }
 }
